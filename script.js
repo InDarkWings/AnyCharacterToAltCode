9918472e3c98525fae41fd7cae7683785422cfcf
InDarkWings <77140029+InDarkWings@users.noreply.github.com>
2023-09-27 13:55:08 +0800

    update2
    2

--- script.js
+++ script.js
@@ -14,17 +14,17 @@ function convertChar() {
         utf8Bytes.push(firstChar.charCodeAt(i));
     }
 
-    var utf8Result = "UTF-8 编码：" + utf8Bytes.join(" ");
-    document.getElementById("utf8Result").innerHTML = utf8Result;
+    // var utf8Result = "UTF-8 编码：" + utf8Bytes.join(" ");
+    // document.getElementById("utf8Result").innerHTML = utf8Result;
 
     // 转换为 Unicode 编码的字符值
-    var unicodeResult = "Unicode 编码：" + firstChar.charCodeAt(0);
-    document.getElementById("unicodeResult").innerHTML = unicodeResult;
+    // var unicodeResult = "Unicode 编码：" + firstChar.charCodeAt(0);
+    // document.getElementById("unicodeResult").innerHTML = unicodeResult;
     //Alt
-    var altCodeResult = "Alt 代码：" + getAltCode(firstChar);
+    var altCodeResult = "Alt码：" + getAltCode(firstChar);
     document.getElementById("altCodeResult").innerHTML = altCodeResult;
     //Test1
-    var Test ="unicodeT111111:" + firstChar.fromCharCode(0);
+    var Test ="Test1:" + firstChar.fromCharCode(0);
     document.getElementById("1").innerHTML = Test;
 }
 
